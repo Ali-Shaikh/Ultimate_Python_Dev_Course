17911b03408c01bdd7f88158cf2d15f0a6f8e276
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-03-25 23:21:38 +0000

    Learn how to get input from user and typecast it

--- PrintStatements.py
+++ PrintStatements.py
@@ -1,5 +1,5 @@
-print (3+4)
+print(3+4)
 
-print (3,4,7)
+print(3,4,7)
 
-print ("This is the addition of 3 + 4 =",3+4)
+print("This is the addition of 3 + 4 =",3+4)
---
+++ UserInput.py
@@ -0,0 +1,8 @@
+name = input("Enter your Name ")
+
+age = int(input("Please enter your age "))
+
+print("Your name is", name, "and you are", age, "years old.")
+
+print(type(name))
+print(type(age))

2cb11dc12629743f6d9b4b16740248e5e6c8a865
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-03-25 01:27:46 +0000

    How to use Print statements and Type Conversions in Python.

---
+++ TypeConversions.py
@@ -0,0 +1,19 @@
+print ("Original value is 7.5")
+
+print ("After converting to int the value is", int(7.5))
+
+print ("Original value is 5")
+
+print ("After converting to floating point the value is", float(5))
+
+print ("Original value is 7.6")
+
+print ("After rounding the value is", round(7.6))
+
+print ("Original value is 7.4")
+
+print ("After rounding the value is", round(7.4))
+
+
+
+
